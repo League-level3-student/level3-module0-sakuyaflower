9ad0870be32da8180b98f0b104ea823349580889
root <root@8ce2e5f2d8d4>
2020-08-29 01:26:18 +0000

    More Array Fun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -11,24 +11,54 @@ public class MoreArrayFun {
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	
-	String[] arrays;
-	arrays = new String[];
+	String[] array = {"hi", "tomato", "silly", "cucumber", "phone"};
+
 	
-	for(int i = 0; i < arrays.length; i++) {
-		
+	for(int i = 0; i < array.length; i++) {
+	//	System.out.println(array[i]);
 	}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
+for(int i = array.length-1; i >=0 ; i--) {
 	
+	//System.out.println(array[i]);
+	}
+
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
+for(int i = 0; i< array.length; i+=2) {
 	
+	//System.out.println(array[i]);
+}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+int numPrinted = 0;
+int[] indexes = new int[array.length];
+for(int i = 0; i < indexes.length; i++) {
+	indexes[i] = -1;
+}
+Random rand = new Random();
+boolean printed = false;
+for(int i = 0; i < array.length; i++) {
+		int answer = rand.nextInt(array.length);
+		for(int r = 0; r < indexes.length; r++ ) {
+			if(indexes[r] == answer) {
+				printed = true;
+				break;
+			}
+		}
+		if(!printed) {
+			System.out.println(array[answer]);
+			indexes[answer] = answer;
+		}
+		else {
+			i--;
+		}
+	printed = false;
+	}
 	
 }
 }
